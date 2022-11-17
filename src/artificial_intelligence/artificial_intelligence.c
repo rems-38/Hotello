8a17c2d2272381b1edf824c04e65e6a0ce96a597
BuildTools <unconfigured@null.spigotmc.org>
2022-11-17 20:12:04 +0100

    first test of evaluation

--- src/artificial_intelligence/artificial_intelligence.c
+++ src/artificial_intelligence/artificial_intelligence.c
@@ -4,11 +4,37 @@
 #include <stdbool.h>
 #include <math.h>
 
-int evaluation(char game_board[8][8], char player) {
-    return get_score(game_board, player); // A chier
+int make_move_count(char game_board[8][8], int target_x, int target_y, char joueur, int moves_origins[8][8][9][2]) {
+    int count = 0;
+    int om_index = moves_origins[target_y][target_x][0][0];
+    for(int i = 1; i < om_index; i++) {
+        int x = moves_origins[target_y][target_x][i][0];
+        int y = moves_origins[target_y][target_x][i][1];
+        int delta_x = target_x - x;
+        int delta_y = target_y - y;
+        int dir_x = delta_x == 0 ? 0 : delta_x / abs(delta_x);
+        int dir_y = delta_y == 0 ? 0 : delta_y / abs(delta_y);
+        for(int i = 1; (0 <= x+i*dir_x && x+i*dir_x < 8) && (0 <= y+i*dir_y && y+i*dir_y < 8); i++) {
+            int move_x = x+(i*dir_x);
+            int move_y = y+(i*dir_y);
+            count++;
+            if(move_x == target_x && move_y == target_y) break;
+        }
+    }
+    return count;
+}   
+
+int evaluation(char game_board[8][8], char player, int best_x, int best_y) {
+    int legal_moves[64][2];
+    int moves_origins[8][8][9][2];
+    int lm_index = compute_legal_moves(game_board, player, legal_moves, moves_origins);
+
+    return make_move_count(game_board, best_x, best_y, player, moves_origins);
+
+    // return get_score(game_board, player); // A chier
 }
 
-int minimax(int depth, char node[8][8], char player, bool maximizingPlayer) {
+int minimax(int depth, char node[8][8], char player, bool maximizingPlayer, int best_x, int best_y) {
     int value;
     if (depth == 0 || is_win(node)) {
         return evaluation(node, player);
