3fae0203b178d1af1ac5d51046e909bbb6e89c2a
Vincent Moucadeau <v.moucadeau@gmail.com>
2022-11-18 18:09:26 +0100

    minimax working

--- src/artificial_intelligence/artificial_intelligence.c
+++ src/artificial_intelligence/artificial_intelligence.c
@@ -2,21 +2,30 @@
 #include "../make_move/make_move.h"
 #include "../utils/utils.h"
 #include <stdbool.h>
-#include <math.h>
 
-int evaluation(char game_board[8][8], char player, int best_x, int best_y) {
+int evaluation(char game_board[8][8], char player) {
     int legal_moves[64][2];
-    char legal_moves_board[8][8];
     int moves_origins[8][8][9][2];
-    int lm_index = compute_legal_moves(game_board, player, legal_moves, moves_origins);
+    char temp_board[8][8];
+    board_copy(game_board, temp_board);
+    int lm_index = compute_legal_moves(temp_board, player, legal_moves, moves_origins);
+    int score = 0;
+    for(int i = 0; i < lm_index; i++) {
+        int x = legal_moves[i][0];
+        int y = legal_moves[i][1];
+        int new_score = make_move(temp_board, player, x, y, moves_origins);
+        if(new_score > score) {
+            score = new_score;
+        }
+    }
 
-    return make_move(game_board, best_x, best_y, player, moves_origins);
+    return score;
 }
 
 int minimax(int depth, char node[8][8], char player, bool maximizingPlayer, int *best_x, int *best_y) {
     int value;
     if (depth == 0 || is_win(node)) {
-        return evaluation(node, player, best_x, best_y);
+        return evaluation(node, player);
     }
     int legal_moves[64][2];
     int moves_origins[8][8][9][2];
@@ -26,19 +35,30 @@ int minimax(int depth, char node[8][8], char player, bool maximizingPlayer, int 
     board_copy(node, origin_board);
 
     if (maximizingPlayer) {
-        value = -INFINITY;
+        value = -10000;
         for (int i = 0; i < lm_index; i++) {
             make_move(node, legal_moves[i][0], legal_moves[i][1], player, moves_origins);
-            value = max(value, minimax(depth - 1, node, player, false, best_x, best_y)); // ici node est comme un child_node (on passe à la branche suivante)
+            int tmp = minimax(depth - 1, node, get_opponent(player), false, best_x, best_y); // on passe au joueur suivant
+            if (tmp > value) {
+                value = tmp;
+                *best_x = legal_moves[i][0];
+                *best_y = legal_moves[i][1];
+            }
+            board_copy(origin_board, node);
         }
     }
     else {
-        value = INFINITY;
+        value = 10000;
         for (int i = 0; i < lm_index; i++) {
             make_move(node, legal_moves[i][0], legal_moves[i][1], player, moves_origins);
-            value = min(value, minimax(depth - 1, node, player, true, best_x, best_y));
+            int tmp = minimax(depth - 1, node, get_opponent(player), true, best_x, best_y); // on passe au joueur suivant
+            if (tmp > value) {
+                value = tmp;
+                *best_x = legal_moves[i][0];
+                *best_y = legal_moves[i][1];
+            }
+            board_copy(origin_board, node);
         }
     }
-    board_copy(origin_board, node);
     return value;
 }
--- src/main.c
+++ src/main.c
@@ -7,11 +7,7 @@
 char game_board[8][8];
 char player = 'O';
 
-
-int main() {
-    init_game_board(game_board);
-    // init_game_board_from_string(game_board, "E6D6C5F4F5F6G5C4D3E3D7C6G4C3C7D8C8F3E8E7G3H4F8H2D2C2F7E1H3H5H1G6H6B8A8G7H8G8G2F2G1E2B3B4F1");
-    
+char pvp(char game_board[8][8]) {
     while (!is_win(game_board)){
         if(!has_legal_move(game_board, player)) player = get_opponent(player);
 
@@ -33,5 +29,42 @@ int main() {
             printf("Illegal move, please try again.\n");
         }
     } 
-    printf("Winner: %c", get_score(game_board, 'X') > get_score(game_board, 'O') ? 'X' : 'O');
+    return get_score(game_board, 'X') > get_score(game_board, 'O') ? 'X' : 'O';
+}
+
+
+char pvai(char game_board[8][8], char human) {
+    while (!is_win(game_board)){
+        if(!has_legal_move(game_board, player)) player = get_opponent(player);
+        int x = 0, y = 0;
+        if(player == human) {
+            char legal_moves_board[8][8];
+            generate_legal_moves_board(game_board, player, legal_moves_board);
+            print_game(legal_moves_board);
+
+            printf("Player %c, please enter your move: ", player);
+            char move[3];
+            scanf("%s", move);
+            
+            x = move[0] - 'A';
+            y = move[1] - '1';
+        }
+        else {
+            minimax(3, game_board, player, true, &x, &y);
+        }
+           
+        if(proceed_move(game_board, player, x, y)) {
+            player = get_opponent(player);
+        }
+        else {
+            printf("Illegal move, please try again.\n");
+        }
+    } 
+    return get_score(game_board, 'X') > get_score(game_board, 'O') ? 'X' : 'O';
+}
+
+int main() {
+    init_game_board(game_board);
+    // init_game_board_from_string(game_board, "E6D6C5F4F5F6G5C4D3E3D7C6G4C3C7D8C8F3E8E7G3H4F8H2D2C2F7E1H3H5H1G6H6B8A8G7H8G8G2F2G1E2B3B4F1");
+    printf("Winner: %c", pvai(game_board, 'O'));
 }
