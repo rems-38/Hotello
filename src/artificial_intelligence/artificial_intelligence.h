02e29166301833e60b9c30e9b43464686db47b62
Vincent Moucadeau <v.moucadeau@gmail.com>
2023-01-12 17:08:42 +0100

    patch depth 7 using the bool maximizingPlayer

--- src/artificial_intelligence/artificial_intelligence.h
+++ src/artificial_intelligence/artificial_intelligence.h
@@ -1,3 +1,3 @@
 int stability(char game_board[8][8], char player);
 int evaluation(char game_board[8][8], char player, int best_x, int best_y);
-void compute_best_move(int depth, char game_board[8][8], char player, int *best_x, int *best_y, int *nb_nodes);
+void compute_best_move(int depth, char game_board[8][8], char player, int *best_x, int *best_y, int *nb_nodes, int playedmoves);
